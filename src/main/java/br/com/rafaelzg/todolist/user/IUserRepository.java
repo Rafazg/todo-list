050f364a5acea41e4ff7d8e7e2cf6de29ea0e582
Rafael Silva <nightmarezlolzin@gmail.com>
2023-10-10 20:14:28 -0300

    database integration

---
+++ src/main/java/br/com/rafaelzg/todolist/user/IUserRepository.java
@@ -0,0 +1,7 @@
+package br.com.rafaelzg.todolist.user;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    UserModel findByUsername(String username);
+}
